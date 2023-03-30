792dd06b4f101bbf7dd721e458765cd8c526ccee
Erik Ballesteros <balleste@mit.edu>
2023-03-30 13:20:42 -0400

    led_control script updated

--- led_control.py
+++ led_control.py
@@ -1,73 +1,25 @@
-import time
-from rpi_ws281x import *
-import argparse
 import board
 import neopixel
-pixels = neopixel.NeoPixel(board.D17, 11)
+import time
 
-def led_initialize(location):
-    #LED strip configuration
-    LED_COUNT = 11
-    LED_PIN = 17
-    LED_FREQ_HZ = 800000
-    LED_DMA = 10
-    LED_BRIGHTNESS = 100
-    LED_INVERT = False
-    LED_CHANNEL = 0
-    if location == "internal":
-        LED_COUNT = 11
-        LED_PIN = 17
-        print("Internal LED Initialized")
-    if location == "right_eye" or "left_eye":
-        LED_COUNT = 5
-    if location == "right_eye":
-        LED_PIN = 27
-        print("Right Eye LED Initialized")
-    if location == "left_eye":
-        LED_PIN = 22
-        print("Left Eye LED Initialized")
+Num_Int = 12
+Num_Eye = 6
 
-def colorWipe(strip, color, wait_ms=50):
-    """Wipe color across display a pixel at a time."""
-    for i in range(strip.numPixels()):
-        strip.setPixelColor(i, color)
-        strip.show()
-        time.sleep(wait_ms/1000.0)
+def interior():
+    for k in range(0,5,1):
+        pixels_int = neopixel.NeoPixel(board.D12, Num_Int, auto_write=False, brightness=0.1*k)
+        for i in range(Num_Int-1):
+            pixels_int[i] = ((0,0,255))
+            i = i+1
+        pixels_int.show()
+        time.sleep(0.1)
 
-def theaterChase(strip, color, wait_ms=50, iterations=10):
-    """Movie theater light style chaser animation."""
-    for j in range(iterations):
-        for q in range(3):
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, color)
-            strip.show()
-            time.sleep(wait_ms/1000.0)
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, 0)
-
-def led_startup(location):
-    if __name__ == '__main__':
-        pixels.fill((0,255,0))
-#        # Process arguments
-#        parser = argparse.ArgumentParser()
-#        parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
-#        args = parser.parse_args()
-#
-#        # Create NeoPixel object with appropriate configuration.
-#        strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-#        # Intialize the library (must be called once before other functions).
-#        strip.begin()
-#
-#        print ('Press Ctrl-C to quit.')
-#        if not args.clear:
-#            print('Use "-c" argument to clear LEDs on exit')
-#
-#        try:
-#
- #           while True:
- #               theaterChase(strip, Color(  0,   0, 255))  # Green theater chase
- #               colorWipe(strip, Color(0, 0, 255))  # Green wipe
- #               
- #       except KeyboardInterrupt:
- #           if args.clear:
- #               colorWipe(strip, Color(0,0,0), 10)
+def eyes():
+    pixels = neopixel.NeoPixel(board.D18, Num_Eye)
+    while True:
+        for i in range(Num_Eye-1):
+            pixels[i] = ((0,0,255))
+            time.sleep(0.1)
+        for i in range(Num_Eye-1):
+            pixels[i] = ((0,0,0))
+            time.sleep(0.1)
--- shell.py
+++ shell.py
@@ -1,27 +1,30 @@
 #Shell script to run OCTAVIUS platform
 
-from led_control import led_initialize
-from led_control import led_startup
+from led_control import interior eyes
 
-import odrive
-from odrive.enums import *
+#import odrive
+import threading
+#from odrive.enums import *
 
-#Initialize LED arrays
-led_initialize("internal")
-led_startup("internal")
-led_initialize("right_eye")
-led_startup("right_eye")
-led_initialize("left_eye")
-led_startup("left_eye")
+if __name__ = "__main__":
 
-#Initialize ODrives
-#Odrive S/Ns as follows:
-# 20563882304E
-# 2071388D304E
-# 208637853548
+    #Initialize LED arrays
+    interior()
+    eyes_light = threading.Thread(target=eyes)
 
-#Assign ODrives wrt motor map
-#Note: M0 -> Right, M1 -> Left
-#Note: odrv0 -> J0, odrv1 -> J1, odrv2 -> J2
+    #Initialize ODrives
+    #Odrive S/Ns as follows:
+    # 20563882304E
+    # 2071388D304E
+    # 208637853548
 
-#Configure each motor
+    #Assign ODrives wrt motor map
+    #Note: M0 -> Right, M1 -> Left
+    #Note: odrv0 -> J0, odrv1 -> J1, odrv2 -> J2
+
+    #Configure each motor
+
+    #Execute Threads
+    eyes_light.start()
+
+    eyes_light.join()
