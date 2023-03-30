f0ae700445da932696acc03c0389a3d846e2137c
Erik Ballesteros <balleste@mit.edu>
2023-03-30 14:49:58 -0400

    next full init

--- shell.py
+++ shell.py
@@ -13,11 +13,11 @@ if __name__ == "__main__":
 
     #Initialize ODrives
     #Odrive S/Ns as follows:
-    # 20563882304E
+    # 20563882304E JOINT 0
     # 2071388D304E
     # 208637853548
-    board_0_driver = Odrive(usb_serial = '20563882304E', axes = [True, True], kp = [0.001, 10], kd = [0.001, 0.001], full_init = True)
-    board_1_driver = Odrive(usb_serial = '2071388D304E', axes = [True, True], kp = [10, 10], kd = [0.001, 0.001], full_init = False)
+    board_0_driver = Odrive(usb_serial = '20563882304E', axes = [True, True], kp = [0.001, 10], kd = [0.001, 0.001], full_init = False)
+    board_1_driver = Odrive(usb_serial = '2071388D304E', axes = [True, True], kp = [10, 10], kd = [0.001, 0.001], full_init = True)
     board_2_driver = Odrive(usb_serial = '208637853548', axes = [True, True], kp = [10, 10], kd = [0.001, 0.001], full_init = False)
 
     print('The motor driver has completed connection and calibration. It is ready to run.')
