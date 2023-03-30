31e3869e1122ee862ee776af50b6aa89763117d4
Erik Ballesteros <balleste@mit.edu>
2023-03-30 15:53:27 -0400

    full calibration

--- shell.py
+++ shell.py
@@ -6,8 +6,8 @@ from OdrivePythonController_Superlimb import Odrive
 import threading
 
 def motor_cmd():
-    board_0_driver.pos_move([0,0])
-    board_1_driver.pos_move([0,50])
+    #board_0_driver.pos_move([0,0])
+    #board_1_driver.pos_move([0,50])
 
 if __name__ == "__main__":
 
@@ -20,9 +20,9 @@ if __name__ == "__main__":
     # 20563882304E JOINT 0
     # 2071388D304E
     # 208637853548
-    board_0_driver = Odrive(usb_serial = '20563882304E', axes = [True, True], kp = [10, 10], kd = [0.001, 0.001], full_init = False)
-    board_1_driver = Odrive(usb_serial = '2071388D304E', axes = [True, True], kp = [10, 10], kd = [0.001, 0.001], full_init = False)
-    board_2_driver = Odrive(usb_serial = '208637853548', axes = [True, True], kp = [10, 10], kd = [0.001, 0.001], full_init = False)
+    board_0_driver = Odrive(usb_serial = '20563882304E', axes = [True, True], kp = [10, 10], kd = [0.001, 0.001], full_init = True)
+    board_1_driver = Odrive(usb_serial = '2071388D304E', axes = [True, True], kp = [10, 10], kd = [0.001, 0.001], full_init = True)
+    board_2_driver = Odrive(usb_serial = '208637853548', axes = [True, True], kp = [10, 10], kd = [0.001, 0.001], full_init = True)
 
     print('The motor driver has completed connection and calibration. It is ready to run.')
 
