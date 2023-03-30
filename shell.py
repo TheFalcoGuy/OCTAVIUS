d8541c1350229674b0e3c9d0d44aab4ce8195e1e
Erik Ballesteros <balleste@mit.edu>
2023-03-30 15:14:55 -0400

    Pos move joint 0 right to angle 0 (DANGER)

--- shell.py
+++ shell.py
@@ -5,6 +5,9 @@ from led_control import eyes
 from OdrivePythonController_Superlimb import Odrive
 import threading
 
+def motor_cmd():
+    board_0_driver.pos_move([0,0])
+
 if __name__ == "__main__":
 
     #Initialize LED arrays
@@ -16,19 +19,21 @@ if __name__ == "__main__":
     # 20563882304E JOINT 0
     # 2071388D304E
     # 208637853548
-    board_0_driver = Odrive(usb_serial = '20563882304E', axes = [True, True], kp = [0.001, 10], kd = [0.001, 0.001], full_init = True)
-    board_1_driver = Odrive(usb_serial = '2071388D304E', axes = [True, True], kp = [10, 10], kd = [0.001, 0.001], full_init = True)
-    board_2_driver = Odrive(usb_serial = '208637853548', axes = [True, True], kp = [10, 10], kd = [0.001, 0.001], full_init = True)
+    board_0_driver = Odrive(usb_serial = '20563882304E', axes = [True, True], kp = [10, 10], kd = [0.001, 0.001], full_init = False)
+    board_1_driver = Odrive(usb_serial = '2071388D304E', axes = [True, True], kp = [10, 10], kd = [0.001, 0.001], full_init = False)
+    board_2_driver = Odrive(usb_serial = '208637853548', axes = [True, True], kp = [10, 10], kd = [0.001, 0.001], full_init = False)
 
     print('The motor driver has completed connection and calibration. It is ready to run.')
+
+    control = threading.Thread(target=motor_cmd)
 
     #Assign ODrives wrt motor map
     #Note: M0 -> Right, M1 -> Left
     #Note: odrv0 -> J0, odrv1 -> J1, odrv2 -> J2
 
-    #Configure each motor
-
     #Execute Threads
     eyes_light.start()
+    control.start()
 
     eyes_light.join()
+    control.join()
