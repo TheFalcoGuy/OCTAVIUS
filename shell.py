fdc5c9c8510aca3d2c437a4c052d88733e85cab3
Erik Ballesteros <balleste@mit.edu>
2023-05-24 12:41:26 -0400

    test again

--- shell.py
+++ shell.py
@@ -7,7 +7,7 @@ import threading
 
 def motor_cmd():
     #board_0_driver.pos_move([0,50])
-    board_1_driver.pos_move([0,50])
+    board_1_driver.pos_move([0,-100])
     #board_2_driver.pos_move([0,-50])
     print('Complete Motion 1')
 
