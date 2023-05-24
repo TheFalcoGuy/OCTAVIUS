c28219214cc47cf41a36900d02470c221b489dee
Erik Ballesteros <balleste@mit.edu>
2023-05-24 10:51:36 -0400

    Switched LED addresses

--- led_control.py
+++ led_control.py
@@ -7,7 +7,7 @@ Num_Eye = 6
 
 def interior():
     for k in range(0,5,1):
-        pixels_int = neopixel.NeoPixel(board.D12, Num_Int, auto_write=False, brightness=0.1*k)
+        pixels_int = neopixel.NeoPixel(board.D18, Num_Int, auto_write=False, brightness=0.1*k)
         for i in range(Num_Int-1):
             pixels_int[i] = ((0,0,255))
             i = i+1
@@ -15,7 +15,7 @@ def interior():
         time.sleep(0.1)
 
 def eyes():
-    pixels = neopixel.NeoPixel(board.D18, Num_Eye)
+    pixels = neopixel.NeoPixel(board.D12, Num_Eye)
     while True:
         for i in range(Num_Eye-1):
             pixels[i] = ((0,0,255))
