207a423d7ae969ab9ad84d165b7070332493247d
Erik Ballesteros <balleste@mit.edu>
2023-05-24 11:06:15 -0400

    Activating motor command (unclear what will happen)

--- shell.py
+++ shell.py
@@ -5,9 +5,9 @@ from led_control import eyes
 from OdrivePythonController_Superlimb import Odrive
 import threading
 
-#def motor_cmd():
-    #board_0_driver.pos_move([0,0])
-    #board_1_driver.pos_move([0,50])
+def motor_cmd():
+    board_0_driver.pos_move([0,0])
+    board_1_driver.pos_move([0,50])
 
 if __name__ == "__main__":
 
@@ -18,15 +18,15 @@ if __name__ == "__main__":
     #Initialize ODrives
     #Odrive S/Ns as follows:
     # 20563882304E JOINT 0
-    # 2071388D304E
-    # 208637853548
+    # 2071388D304E JOINT 1
+    # 208637853548 JOINT 2
     board_0_driver = Odrive(usb_serial = '20563882304E', axes = [True, True], kp = [10, 10], kd = [0.001, 0.001], full_init = False)
     board_1_driver = Odrive(usb_serial = '2071388D304E', axes = [True, True], kp = [10, 10], kd = [0.001, 0.001], full_init = False)
     board_2_driver = Odrive(usb_serial = '208637853548', axes = [True, True], kp = [10, 10], kd = [0.001, 0.001], full_init = False)
 
     print('The motor driver has completed connection and calibration. It is ready to run.')
 
-    #control = threading.Thread(target=motor_cmd)
+    control = threading.Thread(target=motor_cmd)
 
     #Assign ODrives wrt motor map
     #Note: M0 -> Right, M1 -> Left
@@ -34,7 +34,7 @@ if __name__ == "__main__":
 
     #Execute Threads
     eyes_light.start()
-    #control.start()
+    control.start()
 
     eyes_light.join()
-    #control.join()
+    control.join()
